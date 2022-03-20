d6a6c2dfe9b27a86d13f2f81c3eccef30abb2c1f
Rail Sakhaviev <sahaviev@gmail.com>
2022-03-20 16:27:40 +0400

    TASK-28: update ansible playbook configuration, add Fedora host, update run.sh script

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
 
 docker run --name centos7 -d pycontribs/centos:7 sleep 60000000000
 docker run --name ubuntu -d pycontribs/ubuntu:latest sleep 60000000000
+docker run --name fedora -d pycontribs/fedora:latest sleep 60000000000
 
-ansible-playbook -i inventory/test.yml site.yml
+ansible-playbook -i inventory/prod.yml site.yml --ask-vault-password
+
+docker stop centos7 && docker rm centos7
+docker stop ubuntu && docker rm ubuntu
+docker stop fedora && docker rm fedora
